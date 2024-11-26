c32c144d52a52d8083721511a9b7bbad95f7b387
Felix Icaza <fx.joliett17@gmail.com>
2024-11-25 19:54:46 -0600

    feat: add checkout page with initial styles and scripts

---
+++ checkout/script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // select menú
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter) => {
+    const minusButton = counter.querySelector(".btn-minus");
+    const plusButton = counter.querySelector(".btn-plus");
+    const input = counter.querySelector("input");
+
+    minusButton.addEventListener("click", () => {
+      let value = parseInt(input.value);
+      if (value > 0) {
+        input.value = value - 1;
+      }
+    });
+
+    plusButton.addEventListener("click", () => {
+      let value = parseInt(input.value);
+      input.value = value + 1;
+    });
+  });
+
+  // checkout
+  const timeSlots = document.querySelectorAll(".time-slot");
+
+  timeSlots.forEach((slot) => {
+    slot.addEventListener("click", function () {
+      // Deselect other slots
+      timeSlots.forEach((slot) => slot.classList.remove("selected"));
+
+      // Select current slot
+      this.classList.add("selected");
+      console.log(`Selected time: ${this.textContent.trim()}`);
+    });
+  });
+
+  //
+  const selectElement = document.getElementById("mySelect");
+  const guestsContainer = document.querySelector(".guests");
+
+  if (selectElement && guestsContainer) {
+    // Detectar cuando el select se abre (focus) y se cierra (blur)
+    selectElement.addEventListener("focus", () => {
+      guestsContainer.classList.add("open");
+    });
+
+    selectElement.addEventListener("blur", () => {
+      guestsContainer.classList.remove("open");
+    });
+  }
+
+  // Selecciona el elemento .footer-nav
+  const footerNav = document.querySelector(".footer-nav");
+
+  // Verifica si tiene un solo hijo
+  if (footerNav && footerNav.children.length === 1) {
+    footerNav.classList.add("one-btn");
+  }
+
+  document.querySelectorAll(".btn-what-includes").forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault(); // Evita el comportamiento por defecto del enlace
+
+      // Encuentra el `.submenu-item` más próximo al botón
+      const submenu = button.closest(".menu-item").nextElementSibling;
+
+      // Alterna la clase 'active' para mostrar u ocultar el `.submenu-item`
+      submenu.classList.toggle("active");
+    });
+  });
+});
